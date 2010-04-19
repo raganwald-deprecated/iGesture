76b1566a2bccf0ea024889d84ba1376a73fd17b1
Reg Braithwaite <raganwald@gmail.com>
2010-04-18 20:14:59 -0400

    Closes #21

---
+++ drag.js
@@ -0,0 +1,55 @@
+$(document).ready(function() {
+	// pick an image, any image
+	var image_number = Math.floor(Math.random() * 10);
+	
+	$('.viewport img')
+		.attr('src', 'star_wars/' + image_number + '.jpeg');
+	
+	var gesture_mode;
+	var dragscroll_mode;
+	
+	gesture_mode = function () {
+		$('.viewport img')
+			.gesture(['left', 'right', 'hold'])
+			.bind({
+				'gesture_right.drag': function () {
+					image_number = ++image_number % 10;
+					$(this)
+						.attr('src', 'star_wars/' + image_number + '.jpeg');
+					return false;
+				},
+				'gesture_left.drag': function () {
+					image_number = (--image_number + 10) % 10;
+					$(this)
+						.attr('src', 'star_wars/' + image_number + '.jpeg');
+					return false;
+				},
+				'gesture_hold.drag': function (event) {
+					dragscroll_mode(); // probably need to fake a mousedown upwards
+					$(this)
+						.effect("shake", { times:3 }, 100, function () {
+							$(this)
+								.parent()
+									.trigger(event.gesture_data.originalEvent);
+						})
+				}
+			});
+		$('.viewport')
+			.removedragscrollable()
+			.unbind('.drag');
+	}
+	
+	dragscroll_mode = function () {
+		$('.viewport')
+			.dragscrollable({dragSelector: '.dragger:first'})
+			.bind('mouseup.drag', function () {
+				gesture_mode();
+				return false;
+			});
+		$('.viewport img')
+			.removegesture()
+			.unbind('.drag');
+	};
+	
+	gesture_mode();
+});
