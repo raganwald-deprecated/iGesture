677352915a229da20d2c4b077e3186ae3d656317
Reg Braithwaite <raganwald@gmail.com>
2010-04-28 15:51:57 -0400

    Closes #36

--- igesture.jquery.mobile_safari.js
+++ igesture.jquery.mobile_safari.js
@@ -133,7 +133,7 @@ jQuery.fn.gesture = function (events) {
 				x: -1,
 				y: -1,
 				lastmove: "",
-				continuesmode: false,
+				continuesmode: settings.continuesmode,
 				getMoveNameAt: function (i) {
 					switch (Number(this.moves.charAt(i))) {
 					case 1:
@@ -382,9 +382,11 @@ jQuery.fn.gesture = function (events) {
 		
 		var gesture_handler = function (e) {
 			
-			var gesture = {};
-		
-			gesture.target = e.target;
+			var gesture = {
+				target: $(e.target),
+				originalEvent: e,
+				continuesmode: settings.continuesmode
+			};
 
 			// disable browser context menu.
 			if (settings.disablecontextmenu) {
